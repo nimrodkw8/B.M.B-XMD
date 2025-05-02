6e822eb3b587f346f52eac51a1ed4ddedc29f377
nimrodkw8 <nimtech4@gmail.com>
2025-05-03 00:13:00 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~gPsXwLIa#b7swTsXZa0WUKE7pOt6SXCX0bkTSdStAr0AoL9BWyEE",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
